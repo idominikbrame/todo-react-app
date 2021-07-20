1237b339c1a6b8422571be4bc5cb98dfeadaf33d
Dominik Brame <idominikbrame@gmail.com>
2021-07-20 17:53:04 -0500

    completed assignment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props)
-
     this.state = {
       isClicked: false,
       todos: [],
@@ -15,44 +14,44 @@ class App extends React.Component {
   }
 
   onClickHandler = () => {
+    if(this.state.text === ""){
+      return alert("enter a value")
+    }
     this.setState({
       todos: [...this.state.todos, { id: this.state.todos.length + 1, text: this.state.text }],
       text: ""
     })
-    this.refs.inputField.value=""
+    document.querySelector("#input").value = ""
   }
 
   handleClick = (evt, id) => {
-    console.log(id)
-    const foundIndex = this.state.todos.findIndex(todo => todo.id === id)
-    const copy = [...this.state.todos]
-    copy.splice(foundIndex, 1)
-    this.setState({ todos: copy })
-    console.log(foundIndex)
-    console.log(this)
+    const thisId = this.state.todos.findIndex(todo => todo.id === id)
+    const todoList = [...this.state.todos]
+    todoList.splice(thisId, 1)
+    this.setState({ todos: todoList })
+    console.log(thisId)
   }
 
   handleChange = event => {
     this.setState({
-      text: event.target.value
+      text: event.target.value,
     })
+    
   }
 
   render() {
     return (
       <div className="App">
         <button onClick={this.onClickHandler}>Add todo</button>
-        <input type="text" onChange={this.handleChange} ref="inputField"/>
+        <input id="input" type="text" onChange={this.handleChange}/>
         <div>
           <ul>
             {this.state.todos.map(({ text, id }) => {
               return (
-                <ul>
-                <li>
+                <li key={id}>
                   <h3>{text}</h3>
                   <button onClick={event => this.handleClick(event, id)}>Delete</button>
                 </li>
-                </ul>
               )
             })}
           </ul>
